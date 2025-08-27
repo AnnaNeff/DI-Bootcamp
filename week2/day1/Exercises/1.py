09640b60de523bb3101778997a7dbd792686bd31
AnnaNeff <annanefedova1304@gmail.com>
2025-08-27 23:10:55 +0300

    .

--- week2/day1/Exercises/1.py
+++ week2/day1/Exercises/1.py
@@ -1,11 +1,20 @@
-animals = ["jiraf", "tiger", "elephant"]
-def sort_animals(animals):
-        groups = {}
-        for animal in animals:
-            for animal[0] in groups.keys():
-                if animal[0] in groups.keys():
-                    groups[animal[0]].append(animal)
-                else:
-                    groups[animal[0]] = [animal]
-        return groups
-print(sort_animals(animals))
+
+class Farm:
+    def __init__(self, farm_name, animals = {}):
+        self.name = farm_name
+        self.animals = animals
+    
+    def add_animal(self, animal_type, count = 1):
+        animals = {}
+        if animal_type in animals:
+            animals[animal_type] = int(count) + 1
+        else:
+            animals[animal_type] = count
+
+farm_name = "Farm"
+Farm.add_animal("Cow", 1)
+
+name = "Mcdonald"
+name1 = name + "'s"
+
+print(name1)
