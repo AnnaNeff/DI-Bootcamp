4945fe288d6b014089bedbda0d89ff45233981c8
AnnaNeff <annanefedova1304@gmail.com>
2025-09-29 19:46:34 +0300

    .

--- week6/DC6-1.sql
+++ week6/DC6-1.sql
@@ -5,5 +5,13 @@ VALUES
 ('Emma', 'Watson', '04/15/1990', NULL)
 
 -- Outcome: "ERROR:  null value in column "oscars" of relation "actors" violates not-null constraint"
-	-- "Failing row contains (11, Emma, Watson, 1990-04-15, null)."
+-- 	"Failing row contains (11, Emma, Watson, 1990-04-15, null)."
 
+ALTER TABLE actors
+  ALTER COLUMN oscars DROP NOT NULL
+
+INSERT INTO actors (first_name,last_name,date_of_birth,oscars)
+VALUES
+('Emma', 'Watson', '04/15/1990', NULL)
+
+SELECT * FROM actors
